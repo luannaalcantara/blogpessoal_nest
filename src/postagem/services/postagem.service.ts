21d8f2873af62983f659ebf58f79d9aaba53146a
Luanna Alcantara <luannafalcantara@gmail.com>
2026-04-01 17:29:02 -0300

    feat: inclusão do CRUD no blog

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { Postagem } from "../entities/postagem.entity";
 import { InjectRepository } from "@nestjs/typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 
 
 
@@ -10,11 +10,52 @@ export class PostagemService {
 
     constructor(
         @InjectRepository(Postagem)
-        private readonly postagemRepository: Repository<Postagem>){} // injeção de dependencia 
+        private readonly postagemRepository: Repository<Postagem>) { } // injeção de dependencia 
 
-    async findAll(): Promise<Postagem[]>{
-    return await this.postagemRepository.find() // selet* from tb_postagem;
+    async findAll(): Promise<Postagem[]> {
+        return await this.postagemRepository.find() // selet* from tb_postagem;
 
     }
 
+    async findById(id: number): Promise<Postagem> {
+
+        const postagem = await this.postagemRepository.findOne({
+            where: {
+                id
+            }
+
+        });
+
+        if (!postagem)
+            throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+        return postagem;
+
+    }
+    async findAllByTitulo(titulo: string): Promise<Postagem[]> {
+        return await this.postagemRepository.find({
+            where: {
+                titulo: ILike(`%${titulo}%`)
+            }
+        })
+
+    }
+
+    async create(postagem: Postagem): Promise<Postagem> {
+        return await this.postagemRepository.save(postagem);
+
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+
+        await this.findById(id);
+        return await this.postagemRepository.delete(id);
+
+    }
+
+    async update(postagem: Postagem): Promise<Postagem> {
+        await this.findById(postagem.id)
+        return await this.postagemRepository.save(postagem);
+
+    }
 }
